ba94e4309b451178572e1ff35074fab81a6428c8
Pirata <104320209+bmorcelli@users.noreply.github.com>
2024-02-27 10:48:13 -0300

    Update stickcLED.cpp

--- esp32_marauder/stickcLED.cpp
+++ esp32_marauder/stickcLED.cpp
@@ -4,48 +4,48 @@ void stickcLED::RunSetup() {
     pinMode(STICKC_LED_PIN, OUTPUT);
 
 if (!settings_obj.loadSetting<bool>("EnableLED")) {
-    digitalWrite(STICKC_LED_PIN, HIGH);
+    digitalWrite(STICKC_LED_PIN, LOW);
     return;
 }
 
 delay(50);
 
-  digitalWrite(STICKC_LED_PIN, LOW);
-  delay(500);
   digitalWrite(STICKC_LED_PIN, HIGH);
+  delay(500);
+  digitalWrite(STICKC_LED_PIN, LOW);
   delay(250);
-  digitalWrite(STICKC_LED_PIN, LOW);
-  delay(500);
   digitalWrite(STICKC_LED_PIN, HIGH);
+  delay(500);
+  digitalWrite(STICKC_LED_PIN, LOW);
   delay(250);
-  digitalWrite(STICKC_LED_PIN, LOW);
-  delay(500);
   digitalWrite(STICKC_LED_PIN, HIGH);
+  delay(500);
+  digitalWrite(STICKC_LED_PIN, LOW);
 }
 
 void stickcLED::attackLED() {
   if (!settings_obj.loadSetting<bool>("EnableLED"))
     return;
     
-  digitalWrite(STICKC_LED_PIN, LOW);
-  delay(300);
   digitalWrite(STICKC_LED_PIN, HIGH);
+  delay(300);
+  digitalWrite(STICKC_LED_PIN, LOW);
 }
 
 void stickcLED::sniffLED() {
   if (!settings_obj.loadSetting<bool>("EnableLED"))
     return;
     
-  digitalWrite(STICKC_LED_PIN, LOW);
-  delay(300);
   digitalWrite(STICKC_LED_PIN, HIGH);
+  delay(300);
+  digitalWrite(STICKC_LED_PIN, LOW);
 }
 
 void stickcLED::offLED() {
   if (!settings_obj.loadSetting<bool>("EnableLED"))
     return;
   
-  digitalWrite(STICKC_LED_PIN, HIGH);
+  digitalWrite(STICKC_LED_PIN, LOW);
 }
 
 void stickcLED::main() {
