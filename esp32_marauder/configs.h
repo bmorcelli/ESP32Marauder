44308ff0b42c081c434e1600db60b8dcd88ace12
Pirata <104320209+bmorcelli@users.noreply.github.com>
2024-02-27 15:31:24 -0300

    Update configs.h

--- esp32_marauder/configs.h
+++ esp32_marauder/configs.h
@@ -212,10 +212,18 @@
       #define D_BTN 35
     #endif
 
-    #if (defined(MARAUDER_M5STICKC) || defined(MARAUDER_M5STICKCP2))
+    #if MARAUDER_M5STICKC
       #define L_BTN -1
       #define C_BTN 37
       #define U_BTN -1
+      #define R_BTN -1
+      #define D_BTN 39
+    #endif
+
+    #ifdef MARAUDER_M5STICKCP2
+      #define L_BTN -1
+      #define C_BTN 37
+      #define U_BTN 35
       #define R_BTN -1
       #define D_BTN 39
     #endif
